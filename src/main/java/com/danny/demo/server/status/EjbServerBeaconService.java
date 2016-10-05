6745714a485addda8c281edca5286bc31720e512
Danny Arhire <dani.arhire@gmail.com>
2016-10-04 21:13:39 -0700

    add logging with slf4j

--- src/main/java/com/danny/demo/server/status/EjbServerBeaconService.java
+++ src/main/java/com/danny/demo/server/status/EjbServerBeaconService.java
@@ -1,12 +1,18 @@
 package com.danny.demo.server.status;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ejb.Stateless;
 
 @Stateless
 public class EjbServerBeaconService implements ServerBeaconService {
 
+    private static final Logger logger = LoggerFactory.getLogger(EjbServerBeaconService.class);
+
     @Override
     public ServerBeaconResponse getServerStatus() {
+        logger.info("Retrieving server status");
         return new ServerBeaconResponse("online");
     }
 }
